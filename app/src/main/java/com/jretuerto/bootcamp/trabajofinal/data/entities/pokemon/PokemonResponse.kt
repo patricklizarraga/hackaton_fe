d327579b9314363651858f85c4b18c873519ee7c
Joel Retuerto <jaretuerto@hotmail.com>
2021-05-07 17:00:21 -0500

    ahora si

---
+++ app/src/main/java/com/jretuerto/bootcamp/trabajofinal/data/entities/pokemon/PokemonResponse.kt
@@ -0,0 +1,20 @@
+package com.jretuerto.bootcamp.trabajofinal.data.entities.pokemon
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class PokemonResponse(
+    val id: Int,
+    val name: String,
+    val order: Int,
+    val form_order: Int,
+    val is_default: Boolean,
+    val is_battle_only: Boolean,
+    val is_mega: Boolean,
+    val form_name: String,
+    val pokemon: Pokemon,
+    val sprites: Sprites,
+    val version_group: VersionGroup
+
+) : Parcelable
---
+++ app/src/main/java/com/jretuerto/bootcamp/trabajofinal/data/entities/pokemon/Sprites.kt
@@ -0,0 +1,12 @@
+package com.jretuerto.bootcamp.trabajofinal.data.entities.pokemon
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Sprites(
+    val back_default: String,
+    val back_shiny: String,
+    val front_default: String,
+    val front_shiny: String
+) : Parcelable
